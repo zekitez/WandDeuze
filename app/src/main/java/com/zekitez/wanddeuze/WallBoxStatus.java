e97f28b04b81007f1b8d2f4134404fa5f9d6511f
zekitez <35424390+zekitez@users.noreply.github.com>
2022-07-07 15:36:58 +0200

    - Added preference to display number of received statusses - Changed frequency of status requests - Updated colors of Amps ring to match the Wallbox - Fixed 'Resume' while cable not plugged in - Java: switched from Timer to ScheduledExecutorService due to rare processing halts

---
+++ app/src/main/java/com/zekitez/wanddeuze/WallBoxStatus.java
@@ -0,0 +1,16 @@
+package com.zekitez.wanddeuze;
+
+public class WallBoxStatus {
+
+    static final int STATUS_NOT_CONNECTED = 0;
+    static final int STATUS_READY = 161;
+    static final int STATUS_WAIT_NEXT_SCHEDULE_8 = 178;
+    static final int STATUS_WAIT_NEXT_SCHEDULE_9 = 179;
+    static final int STATUS_WAIT_CAR_DEMAND_0 = 180;
+    static final int STATUS_WAIT_CAR_DEMAND_1 = 181;
+    static final int STATUS_PAUSED = 182;
+    static final int STATUS_CHARGING = 194;
+    static final int STATUS_LOCKED = 209;
+    static final int STATUS_WAIT_UNLOCK = 210;
+
+}
